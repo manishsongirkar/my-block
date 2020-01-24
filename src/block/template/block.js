7942c1a67d49e3095a93ecf692ebc663952b1702
Manish Songirkar <manish.songirkar@rtcamp.com>
2020-01-24 17:23:14 +0530

    First Commit with template block

---
+++ src/block/template/block.js
@@ -0,0 +1,63 @@
+/**
+ * BLOCK: Template Block
+ *
+ * @package My_Block
+ */
+
+//  Import CSS.
+import './editor.scss';
+import './style.scss';
+
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
+
+const { InnerBlocks } = wp.editor;
+
+const allowedBlocks = [
+	'core/paragraph',
+	'core/heading',
+	'core/columns',
+	'core/column',
+	'core/image',
+];
+
+const template = [
+	[ 'core/columns', {}, [
+		[ 'core/column', {}, [
+			[ 'core/image' ],
+		] ],
+		[ 'core/column', {}, [
+			[ 'core/heading', { placeholder: 'Enter title...' } ],
+			[ 'core/paragraph', { placeholder: 'Enter content...' } ],
+		] ],
+	] ],
+];
+
+registerBlockType( 'my-block/template-block', {
+	title: __( 'Template Block' ),
+	icon: 'shield',
+	category: 'common',
+	keywords: [
+		__( 'template block' ),
+		__( 'innertemplate' ),
+	],
+
+	edit: ( props ) => {
+		return (
+			<div className={ props.attributes.className }>
+				<InnerBlocks
+					allowedBlocks={ allowedBlocks }
+					template={ template }
+				/>
+			</div>
+		);
+	},
+
+	save: ( props ) => {
+		return (
+			<div className={ props.attributes.className }>
+				<InnerBlocks.Content />
+			</div>
+		);
+	},
+} );
